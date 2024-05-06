8e76269e787f887ab8fe28d103e7a1390f1e2161
smrh54430 <signe510@hotmail.com>
2024-05-06 12:53:59 +0200

    hi

--- src/PersonligShopper.vue
+++ src/PersonligShopper.vue
@@ -3,5 +3,20 @@
 </script>
 
 <template>
-    <p>Personlig Shopper side</p>
+  <header>
+    <img id="logo" src="@/assets/mackepranglogo.png" alt="mackepranglogo">
+  <nav>
+  <ul>
+  <li><a href="/personligshopper">Personlig Shopper</a></li>
+  <li><a href='/tirsdagsklumme'>Tirsdagsklumme</a></li>
+  <li><a href='/ommig'>Om mig</a></li>
+  <li><a id="bookknap" href='/bookher'>Book her</a></li>
+</ul>
+  </nav>
+  </header>
+
+<video id="shoppersidevideo" autoplay loop muted>
+    <source src="./assets/testvideo.mp4" type="video/mp4">
+</video>
+
 </template>
