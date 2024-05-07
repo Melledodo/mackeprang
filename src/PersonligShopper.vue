ef1b169df2d691a05957d5ac37b143523b484137
smrh54430 <signe510@hotmail.com>
2024-05-07 12:35:09 +0200

    kk

--- src/PersonligShopper.vue
+++ src/PersonligShopper.vue
@@ -16,7 +16,7 @@ import { ref } from 'vue';
     </header>
   
     <video id="shoppersidevideo" autoplay loop muted>
-      <source src="./assets/testvideo.mp4" type="video/mp4">
+      <source src="" type="video/mp4">
     </video>
 
 <div class="konsultation">
@@ -38,7 +38,43 @@ import { ref } from 'vue';
   </div>
 </div>
 
+<div class="psfelt">
+  <div class="ps-container">
+    <div class="pstekst">
+      <h2><b>1 times konsultation hjemme hos dig</b></h2>
+      <p>
+        Du får en kropsanalyse, samt et kig på din personlige stil og garderobe.<br><br>
+        Personlig rådgivning om snit, former og farver der klæder dig godt.<br><br>
+        Du får gode råd til, hvordan du selv sammensætter dit tøj.<br><br>
+        Efterfølgende får du tilsendt materiale med billeder og kropsanalyse samt former, farver og snit der passer til dig.<br>
+      </p>
+      <div class="pris">900,-</div>
+      <button class="bookknap">BOOK HER</button>
+    </div>
+    <div class="psfoto">
+      <img src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg" alt="Billede">
+    </div>
+  </div>
+</div>
 
+<div class="garderobe">
+  <div class="garderobe-container">
+    <div class="garderobetekst">
+      <h2><b>1 times konsultation hjemme hos dig</b></h2>
+      <p>
+        Du får en kropsanalyse, samt et kig på din personlige stil og garderobe.<br><br>
+        Personlig rådgivning om snit, former og farver der klæder dig godt.<br><br>
+        Du får gode råd til, hvordan du selv sammensætter dit tøj.<br><br>
+        Efterfølgende får du tilsendt materiale med billeder og kropsanalyse samt former, farver og snit der passer til dig.<br>
+      </p>
+      <div class="pris">900,-</div>
+      <button class="bookknap">BOOK HER</button>
+    </div>
+    <div class="garderobefoto">
+      <img src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg" alt="Billede">
+    </div>
+  </div>
+</div>
  
     <footer>
         <p>© 2024 Mackeprang</p>
