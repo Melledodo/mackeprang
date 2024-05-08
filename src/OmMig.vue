39f388438a0aa2320c4449b8d8b55cc49f1b8251
smrh54430 <signe510@hotmail.com>
2024-05-08 11:30:00 +0200

    cc

--- src/OmMig.vue
+++ src/OmMig.vue
@@ -1,7 +1,21 @@
 <script setup>
-
+import { ref } from 'vue';
 </script>
 
 <template>
-    <p>Om mig side</p>
-</template>
+  <header id="personligshopper-header">
+      <img id="logo" src="@/assets/mackepranglogo.png" alt="mackepranglogo">
+      <nav>
+        <ul>
+            <li><a href="/personligshopper">Personlig Shopper</a></li>
+            <li><a href='/tirsdagsklumme'>Tirsdagsklumme</a></li>
+            <li><a href='/ommig'>Om mig</a></li>
+            <li><a id="bookmenuknap" href='/bookher'>Book her</a></li>
+        </ul>
+      </nav>
+    </header>
+  
+    <footer>
+        <p>© 2024 Mackeprang</p>
+    </footer>
+    </template>
