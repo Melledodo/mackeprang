a52ad0d802f66c777f072bed815c52bceef07e14
Melledodo <mkaagaard@hotmail.com>
2024-05-09 12:41:04 +0200

    sdx

--- src/TirsdagsKlumme.vue
+++ src/TirsdagsKlumme.vue
@@ -3,5 +3,21 @@
 </script>
 
 <template>
-    <p>Tirsdagsklumme side</p>
+  <header id="personligshopper-header">
+    <a href="/">
+    <img id="logo" src="@/assets/mackepranglogo.png" alt="mackepranglogo">
+    </a>
+      <nav>
+        <ul>
+            <li><a href="/personligshopper">Personlig Shopper</a></li>
+            <li><a href='/tirsdagsklumme'>Tirsdagsklumme</a></li>
+            <li><a href='/ommig'>Om mig</a></li>
+            <li><a id="bookmenuknap" href='/bookher'>Book her</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <footer>
+        
+    </footer>
 </template>
