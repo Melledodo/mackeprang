02f5a929e73a5085f2dacdd7ba69ffa86d39058a
smrh54430 <signe510@hotmail.com>
2024-05-14 13:21:37 +0200

    d

--- src/TirsdagsKlumme.vue
+++ src/TirsdagsKlumme.vue
@@ -1,5 +1,22 @@
 <script setup>
+import { ref } from 'vue';
 
+
+const databaseItems = ref([]);
+
+const requestOptions = {method: 'GET',};
+
+fetch('', requestOptions)
+  .then(response => response.json())
+  .then(data => {
+    databaseItems.value = data;
+    console.log(databaseItems.value);
+
+  })
+  .catch(error => { //arrow function, udført hvis der sker fejl og console.error udskriver i console, error. Det er et promise
+    console.error('Error:', error);
+
+  });
 </script>
 
 <template>
@@ -17,7 +34,31 @@
       </nav>
     </header>
 
+  <div class="klumme-container">
+    <h1>Tirsdagsklumme</h1>
+      <p class="nyesteklumme">tekst fra seneste klumme</p>
+      <div class="database-container">
+        <div class="database-items">
+          <div v-for="(item, index) in databaseItems" :key="index" class="database-item">
+            <h2>{{ item.title }}</h2>
+              <div class="image-boxes">
+                <div v-for="(billede, billedeIndex) in item.billeder" :key="billedeIndex" class="image-box">
+                  <div class="image-wrapper">
+                  <img :src="billede.billede" :alt="'Billede' + (billedeIndex + 1)">
+                  <p>{{ billede.billedtekst }}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+
+
+
+
+
     <footer>
-        
+      <p>© 2024 Mackeprang</p>
     </footer>
 </template>
